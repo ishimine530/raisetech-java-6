af09247e752291ed3c862abe484be0abbe329358
ishimine530 <88490921+ishimine530@users.noreply.github.com>
2022-10-08 03:34:04 +0900

    簡易会員登録機能を追加

--- src/main/java/com/raisetech/controller/HelloWorldController.java
+++ src/main/java/com/raisetech/controller/HelloWorldController.java
@@ -3,12 +3,27 @@ package com.raisetech.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.raisetech.form.Form;
 
 @Controller
 public class HelloWorldController {
+	@ModelAttribute
+	public Form setUpForm() {
+		return new Form();
+	}
+
 	@GetMapping("/")
 	public String helloWorld(Model model) {
 		model.addAttribute("message", "Hello World");
 		return "index";
+	}
+
+	@PostMapping("/")
+	public String confirmView(Form form, Model model) {
+		model.addAttribute("message", "登録完了");
+		return "confirm";
 	}
 }
