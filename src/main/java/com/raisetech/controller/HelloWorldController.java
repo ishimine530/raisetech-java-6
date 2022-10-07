81628490ed13cf1a5328151afedb7f9dfb5e2975
ishimine530 <88490921+ishimine530@users.noreply.github.com>
2022-10-08 04:17:31 +0900

    単項目チェックを追加

--- src/main/java/com/raisetech/controller/HelloWorldController.java
+++ src/main/java/com/raisetech/controller/HelloWorldController.java
@@ -2,6 +2,8 @@ package com.raisetech.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +24,13 @@ public class HelloWorldController {
 	}
 
 	@PostMapping("/")
-	public String confirmView(Form form, Model model) {
+	public String confirmView(@Validated Form form, BindingResult bindingResult, Model model) {
+		model.addAttribute("message", "Hello World");
+
+		if (bindingResult.hasErrors()) {
+			return "index";
+		}
+		
 		model.addAttribute("message", "登録完了");
 		return "confirm";
 	}
--- src/main/java/com/raisetech/form/Form.java
+++ src/main/java/com/raisetech/form/Form.java
@@ -2,16 +2,28 @@ package com.raisetech.form;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Range;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
 @Data
 public class Form {
+	@NotBlank(message = "*Nameが未入力です。")
+	@Size(min = 1, max = 20, message = "*{min}～{max}文字で入力してください。")
 	private String name;
 	
+	@NotNull(message = "*Ageが未入力です。")
+	@Range(min=0, max=200, message = "*{min}～{max}の数字を入力してください。")
 	private Integer age;
 	
+	@NotNull(message = "*Date of birthが未選択です。")
+	@PastOrPresent(message = "*未来の日付は選択できません。")
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
 	private LocalDate birth;
 }
